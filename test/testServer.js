b957ee1478c2718efd0a85631c74888e7ce18545
Yaniv Kessler <yanivk@gmail.com>
2013-11-14 21:48:16 +0200

    gzip upload

---
+++ test/testServer.js
@@ -0,0 +1,22 @@
+var zmq = require('zmq');
+var path = require('path');
+var outgoingWorkChannel = zmq.socket('push');
+outgoingWorkChannel.identity = 'loggerToZeroS3' + process.pid;
+outgoingWorkChannel.bind('tcp://127.0.0.1:5003');
+
+
+setTimeout(send, 5000);
+
+
+function send() {
+
+	var message = {
+		bucket: 'rtb-redshift',
+		key: '1',
+		path: path.join(__dirname, 'uploadtestfile')
+	}
+
+	console.log('sending', message);
+
+	outgoingWorkChannel.send(JSON.stringify(message));
+}
